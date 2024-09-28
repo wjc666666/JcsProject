300bbd490d63d01ec4c076d1a5626e14f3c0ab76
wjc666666 <2101079563@qq.com>
2024-09-28 14:27:46 +0100

    Add task: form added

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Task from './components/Task';
 import React, { useState } from 'react';
+import AddTaskForm from './components/Form';
 
 function App() {
   const [ taskState, setTaskState ] = useState({
@@ -24,20 +25,20 @@ function App() {
     } 
   return (
     <div className="container">
-      <h1>Tasky</h1>
-      {taskState.tasks.map((task, index) => (              
-    <Task 
-      title={task.title}
-      description={task.description}
-      deadline={task.deadline}
-      key={task.id}
-      done={task.done}
-      markDone={() => doneHandler(index)}
-      deleteTask = {() => deleteHandler(index)}
-    />
-  ))}
-  
-    </div>
+    <h1>Tasky</h1>
+    {taskState.tasks.map((task, index) => (              
+      <Task 
+        title={task.title}
+        description={task.description}
+        deadline={task.deadline}
+        key={task.id}
+        done={task.done}
+        markDone={() => doneHandler(index)}
+        deleteTask = {() => deleteHandler(index)}
+      />
+    ))}
+    <AddTaskForm />
+  </div>
   );
  
 }
