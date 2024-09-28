248af39109c5366400c964726861cd9c2f19b2e4
wjc666666 <2101079563@qq.com>
2024-09-28 13:46:51 +0100

    Added task state

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -13,11 +13,9 @@ function App() {
   return (
     <div className="container">
       <h1>Tasky</h1>
-      <Task title="Dishes" deadline="Today" />
-      <Task title="Laundry" deadline="Tomorrow" >
-      Fold laundry and put away
-    </Task>
-      <Task title="Tidy" deadline="Today" />
+      <Task title={taskState.tasks[0].title} deadline={taskState.tasks[0].deadline} description={taskState.tasks[0].description} />
+      <Task title={taskState.tasks[1].title} deadline={taskState.tasks[1].deadline} description={taskState.tasks[1].description} />
+      <Task title={taskState.tasks[2].title} deadline={taskState.tasks[2].deadline} description={taskState.tasks[2].description} />
     </div>
   );
  
--- tasky/src/components/Task.js
+++ tasky/src/components/Task.js
@@ -2,12 +2,11 @@ import React from 'react';
 const Task = (props) => {
     
     return (
-        <div className="container">
-          <h1>Tasky</h1>
-          <Task title={taskState.tasks[0].title} deadline={taskState.tasks[0].deadline} description={taskState.tasks[0].description} />
-          <Task title={taskState.tasks[1].title} deadline={taskState.tasks[1].deadline} description={taskState.tasks[1].description} />
-          <Task title={taskState.tasks[2].title} deadline={taskState.tasks[2].deadline} description={taskState.tasks[2].description} />
+        <div className="card">
+            <p>{props.title}</p>
+            <p>Due: {props.deadline}</p>
+            <p>{props.children}</p>
         </div>
-      );
+    )
 }
 export default Task;
